4f16e4dd7eba50f9668992247a3fa8d100df824d
Isaac Anafo <anafoisaac7076@gmail.com>
2025-10-25 18:08:00 -0700

    Add a brief message about the project

---
+++ GradeMaster.py
@@ -0,0 +1,5 @@
+#A brief message about the project
+print("CODE THE VACATION WITH MORTOTI CHALLENGE 2025")
+print("PROJECT AUTHOR: Isaac Atoeyine Anafo")
+print("PROJECT TITLE: GradeMaster, academic Performance Evaluation Tool")
+print("PROJECT DESCRIPTION: This program calculates students' CWA or GPA based on their course inputs.")
